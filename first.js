fb0ac1be30dd9571fdc22966ccaf6c2e072b3264
anushabansal2007-create <anushabansal2007@gmail.com>
2026-02-02 11:54:11 +0530

    feat:async ,sync, and timer code

--- first.js
+++ first.js
@@ -3,21 +3,27 @@
 // const para=document.getElementById('para');
 // para.textContent='this is pra1';
 // console.log(para);
+
 // const para=document.getElementsByClassName('para')
 // para[0].textContent='this is para1'
 // para[1].style.color='pink'
 // para[1].textContent='this is para2'
 // para[2].style.color='blue'
 // console.log(para);
+
+
 // const para=document.querySelector('.para')
 // para.style.color='green'
 // para.textContent='this is para1'
 // console.log(para);
+
+
 // const para=document.querySelectorAll('.para')
 // para[0].innerText='this is para1'
 // para[0].textContent='this is para1'
 // para[1].style.color='blue'
 // para[1].innerHTML
+
 
 // const button=document.querySelector('#btn')
 // // button.classList.add('btn')
@@ -31,11 +37,13 @@
 //  button2.addEventListener('click',function(){
 //     button.removeEventListener('click',message)
 // //  })
+
+
 // const btn=document.querySelector('#btn')
 // btn.addEventListener('keyup',function(event){
 //     console.log(event.key)})
 
-const handleSubmit=function(event){
+// const handleSubmit=function(event){
 //     event.preventDefault()
 //     console.log('form submitted')
 // }
@@ -43,6 +51,7 @@ const handleSubmit=function(event){
 // const form=document.querySelector('#myFo
 // form.addEventListener('submit',handleSubmit)
 // 
+
 // const btn=document.querySelector("button")
 // const div=document.querySelector(".container")
 
@@ -55,11 +64,12 @@ const handleSubmit=function(event){
 // // bubbling default for false
 // div.addEventListener("click",function(){console.log('.container')},false)
 
-debugger
-console.log(a);
 
-var a = 78   // part of global scope
-let b = 345 //  stored in script scope
+// debugger
+// console.log(a);
+
+// var a = 78   // part of global scope
+// let b = 345 //  stored in script scope
 
 // console.log(a);
 // console.log(b);
@@ -70,6 +80,7 @@ let b = 345 //  stored in script scope
 //     console.log("inside fn");
 // }
 // print()
+
 
 // let total = 100
 // function calculate(){
@@ -119,3 +130,72 @@ let b = 345 //  stored in script scope
 //     console.log(count)
 //     count++;
 // },1000);
+
+
+// 2 feb
+
+
+// function print(){  //callback function
+//     console.log("hello students!!");
+// }
+
+// function greet(num){ //higher order function
+//     console.log("welcome to js class");
+//     num()
+// }
+// greet(print)
+
+
+// function print(name){
+//     console.log("hello students!!", name);
+// }
+// function greet (num){
+//     console.log("welcome to js class");
+//     setTimeout(() => {
+//         console.log("Inside SetTimeout")
+//         let firstName="Anusha"
+//         num(firstName)
+//     }, 2000)
+// }
+// greet(print)
+
+
+// console.log("starting homework....");
+
+// setTimeout(() => {
+//     console.log("homework done!")
+//     console.log("starting dinner..")
+//     setTimeout(() => {
+//         console.log("dinner done!!")
+//         console.log("getting ready to go out..")
+//         setTimeout(() => {
+//             console.log("going to the playground...")
+//         }, 1000); //after dinner
+//     }, 1500);  //dinner time
+// }, 2000); //homework time
+
+
+// function finishHomework(callback){
+//     console.log("starting homework...")
+//     setTimeout(() => {
+//         console.log("Homework done!!")
+//         callback();
+//     }, 2000);
+// }
+// function eatDinner(callback){
+//     console.log("Starting dinner...")
+//     setTimeout(() => {
+//         console.log("Dinner done!!")
+//         callback();
+//     }, 1500);
+// }
+// function goToPlayground(){
+//     console.log("Going to the playground...")
+// }
+// finishHomework(()=>{
+//     eatDinner(()=>{
+//         goToPlayground();
+//     });
+// });
+
+
